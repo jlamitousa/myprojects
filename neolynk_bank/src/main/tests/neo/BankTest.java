34a539ca832450c98c5773dd395ad6ad14c43cb3
jeanluc.amitousa.mankoy <jeanluc.amitousa.mankoy@gmail.com>
2017-06-21 00:00:29 +0200

    Remove duplication (should be done at previous step ...)

--- neolynk_bank/src/main/tests/neo/BankTest.java
+++ neolynk_bank/src/main/tests/neo/BankTest.java
@@ -1,26 +1,31 @@
 package neo;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class BankTest {
 
+	Bank b;
+
+	@Before
+	public void init() {
+		this.b = new Bank();
+	}
+	
 	@Test
 	public void getNumberOfUserInit() {
-		Bank b = new Bank();
 		Assert.assertEquals(0, b.getNumberOfUser());
 	}
 	
 	@Test
 	public void getNumberOfUserAfterAdd() {
-		Bank b = new Bank();
 		b.addUser();
 		Assert.assertEquals(1, b.getNumberOfUser());
 	}
 	
 	@Test
 	public void getNumberOfUserAfterDelete() {
-		Bank b = new Bank();
 		b.addUser();
 		b.deleteUser();
 		Assert.assertEquals(0, b.getNumberOfUser());
